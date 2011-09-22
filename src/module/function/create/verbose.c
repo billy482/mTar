fc4b2e1c61275c50c4baca8c05b4889c82bb2f8f
Clercin Guillaume <clercin.guillaume@gmail.com>
2011-09-22 10:35:23 +0200

    Update header licence

--- src/module/function/create/verbose.c
+++ src/module/function/create/verbose.c
@@ -1,31 +1,34 @@
-/***********************************************************************\
-*                               ______                                  *
-*                          __ _/_  __/__ _____                          *
-*                         /  ' \/ / / _ `/ __/                          *
-*                        /_/_/_/_/  \_,_/_/                             *
-*                                                                       *
-*  -------------------------------------------------------------------  *
-*  This file is a part of mTar                                          *
-*                                                                       *
-*  mTar is free software; you can redistribute it and/or                *
-*  modify it under the terms of the GNU General Public License          *
-*  as published by the Free Software Foundation; either version 3       *
-*  of the License, or (at your option) any later version.               *
-*                                                                       *
-*  This program is distributed in the hope that it will be useful,      *
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-*  GNU General Public License for more details.                         *
-*                                                                       *
-*  You should have received a copy of the GNU General Public License    *
-*  along with this program; if not, write to the Free Software          *
-*  Foundation, Inc., 51 Franklin Street, Fifth Floor,                   *
-*  Boston, MA  02110-1301, USA.                                         *
-*                                                                       *
-*  -------------------------------------------------------------------  *
-*  Copyright (C) 2011, Clercin guillaume <clercin.guillaume@gmail.com>  *
-*  Last modified: Sun, 18 Sep 2011 23:20:05 +0200                       *
-\***********************************************************************/
+/***************************************************************************\
+*                                  ______                                   *
+*                             __ _/_  __/__ _____                           *
+*                            /  ' \/ / / _ `/ __/                           *
+*                           /_/_/_/_/  \_,_/_/                              *
+*                                                                           *
+*  -----------------------------------------------------------------------  *
+*  This file is a part of mTar                                              *
+*                                                                           *
+*  mTar is free software; you can redistribute it and/or                    *
+*  modify it under the terms of the GNU General Public License              *
+*  as published by the Free Software Foundation; either version 3           *
+*  of the License, or (at your option) any later version.                   *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+*  GNU General Public License for more details.                             *
+*                                                                           *
+*  You should have received a copy of the GNU General Public License        *
+*  along with this program; if not, write to the Free Software              *
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor,                       *
+*  Boston, MA  02110-1301, USA.                                             *
+*                                                                           *
+*  You should have received a copy of the GNU General Public License        *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                           *
+*  -----------------------------------------------------------------------  *
+*  Copyright (C) 2011, Clercin guillaume <clercin.guillaume@gmail.com>      *
+*  Last modified: Thu, 22 Sep 2011 10:21:37 +0200                           *
+\***************************************************************************/
 
 // strcat, strcpy
 #include <string.h>
