7764005ead9242771df3c8fbc8570fd17f83a6d3
Clercin Guillaume <clercin.guillaume@gmail.com>
2012-05-05 15:13:28 +0200

    Move mtar/plugin.c

--- src/mtar/loader.h
+++ src/mtar/loader.h
@@ -7,7 +7,7 @@
 *  -----------------------------------------------------------------------  *
 *  This file is a part of mTar                                              *
 *                                                                           *
-*  mTar is free software; you can redistribute it and/or                    *
+*  mTar (modular tar) is free software; you can redistribute it and/or      *
 *  modify it under the terms of the GNU General Public License              *
 *  as published by the Free Software Foundation; either version 3           *
 *  of the License, or (at your option) any later version.                   *
@@ -26,7 +26,7 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
 *                                                                           *
 *  -----------------------------------------------------------------------  *
-*  Copyright (C) 2011, Clercin guillaume <clercin.guillaume@gmail.com>      *
+*  Copyright (C) 2012, Clercin guillaume <clercin.guillaume@gmail.com>      *
 *  Last modified: Tue, 25 Oct 2011 09:21:20 +0200                           *
 \***************************************************************************/
 
