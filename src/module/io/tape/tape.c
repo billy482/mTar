351eb58f95df686b84057b128a253cecf8b48809
Clercin Guillaume <clercin.guillaume@gmail.com>
2012-05-07 23:17:21 +0200

    Move module io tape

--- src/module/io/tape/tape.c
+++ src/module/io/tape/tape.c
@@ -7,7 +7,7 @@
 *  -----------------------------------------------------------------------  *
 *  This file is a part of mTar                                              *
 *                                                                           *
-*  mTar is free software; you can redistribute it and/or                    *
+*  mTar (modular tar) is free software; you can redistribute it and/or      *
 *  modify it under the terms of the GNU General Public License              *
 *  as published by the Free Software Foundation; either version 3           *
 *  of the License, or (at your option) any later version.                   *
@@ -26,9 +26,11 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
 *                                                                           *
 *  -----------------------------------------------------------------------  *
-*  Copyright (C) 2011, Clercin guillaume <clercin.guillaume@gmail.com>      *
-*  Last modified: Mon, 31 Oct 2011 15:25:53 +0100                           *
+*  Copyright (C) 2012, Clercin guillaume <clercin.guillaume@gmail.com>      *
+*  Last modified: Mon, 07 May 2012 23:03:14 +0200                           *
 \***************************************************************************/
+
+#include <mtar-io-tape.chcksum>
 
 #include <mtar/verbose.h>
 
@@ -36,12 +38,17 @@
 
 static void mtar_io_tape_init(void) __attribute__((constructor));
 static void mtar_io_tape_show_description(void);
+static void mtar_io_tape_show_version(void);
 
 static struct mtar_io mtar_io_tape = {
 	.name             = "tape",
+
 	.new_in           = mtar_io_tape_new_in,
 	.new_out          = mtar_io_tape_new_out,
+
 	.show_description = mtar_io_tape_show_description,
+	.show_version     = mtar_io_tape_show_version,
+
 	.api_version      = MTAR_IO_API_VERSION,
 };
 
@@ -52,5 +59,10 @@ void mtar_io_tape_init() {
 
 void mtar_io_tape_show_description() {
 	mtar_verbose_print_help("tape : used for tape (scsi tape device)");
+}
+
+void mtar_io_tape_show_version() {
+	mtar_verbose_printf("  tape : used for tape (scsi tape device) (version: " MTAR_VERSION ")\n");
+	mtar_verbose_printf("         SHA1 of source files: %s\n", MTAR_IO_TAPE_SRCSUM);
 }
 
