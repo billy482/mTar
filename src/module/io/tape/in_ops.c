33519c7cb3505a58ee26a4df766538927afb8b51
Clercin Guillaume <clercin.guillaume@gmail.com>
2012-05-10 00:00:59 +0200

    Various bug fixes

--- src/module/io/tape/in_ops.c
+++ src/module/io/tape/in_ops.c
@@ -27,7 +27,7 @@
 *                                                                           *
 *  -----------------------------------------------------------------------  *
 *  Copyright (C) 2012, Clercin guillaume <clercin.guillaume@gmail.com>      *
-*  Last modified: Wed, 09 May 2012 21:38:18 +0200                           *
+*  Last modified: Thu, 10 May 2012 00:00:42 +0200                           *
 \***************************************************************************/
 
 // errno
@@ -53,7 +53,6 @@ struct mtar_io_tape_in {
 	int last_errno;
 
 	char * buffer;
-	unsigned int buffer_size;
 	unsigned int block_size;
 	char * buffer_pos;
 };
@@ -79,7 +78,7 @@ static struct mtar_io_in_ops mtar_io_tape_in_ops = {
 
 ssize_t mtar_io_tape_in_block_size(struct mtar_io_in * io) {
 	struct mtar_io_tape_in * self = io->data;
-	return self->buffer_size;
+	return self->block_size;
 }
 
 int mtar_io_tape_in_close(struct mtar_io_in * io) {
@@ -103,13 +102,13 @@ off_t mtar_io_tape_in_forward(struct mtar_io_in * io, off_t offset) {
 
 	ssize_t nb_total_read = self->block_size - (self->buffer_pos - self->buffer);
 	if (nb_total_read > 0) {
-		ssize_t will_copy = nb_total_read > offset ? offset : nb_total_read;
+		ssize_t will_move = nb_total_read > offset ? offset : nb_total_read;
 
-		self->buffer_pos += will_copy;
-		self->position += will_copy;
+		self->buffer_pos += will_move;
+		self->position += will_move;
 
-		if (will_copy == offset)
-			return offset;
+		if (will_move == offset)
+			return self->position;
 	}
 
 	if (offset - nb_total_read >= self->block_size) {
@@ -138,10 +137,9 @@ off_t mtar_io_tape_in_forward(struct mtar_io_in * io, off_t offset) {
 		ssize_t will_move = offset - nb_total_read;
 		self->buffer_pos = self->buffer + will_move;
 		self->position += will_move;
-		return self->position;
-	} else {
-		return self->position;
 	}
+
+	return self->position;
 }
 
 void mtar_io_tape_in_free(struct mtar_io_in * io) {
@@ -202,7 +200,7 @@ ssize_t mtar_io_tape_in_read(struct mtar_io_in * io, void * data, ssize_t length
 		return nb_read;
 	} else if (nb_read > 0) {
 		ssize_t will_copy = length - nb_total_read;
-		memcpy(data, self->buffer, will_copy);
+		memcpy(c_data + nb_total_read, self->buffer, will_copy);
 		self->buffer_pos = self->buffer + will_copy;
 		self->position += will_copy;
 		return length;
@@ -218,9 +216,8 @@ struct mtar_io_in * mtar_io_tape_new_in(int fd, int flags __attribute__((unused)
 	data->last_errno = 0;
 
 	data->buffer = malloc(option->block_factor << 9);
-	data->buffer_size = option->block_factor << 9;
-	data->block_size = 0;
-	data->buffer_pos = data->buffer;
+	data->block_size = option->block_factor << 9;
+	data->buffer_pos = data->buffer + data->block_size;
 
 	struct mtar_io_in * io = malloc(sizeof(struct mtar_io_in));
 	io->ops = &mtar_io_tape_in_ops;
