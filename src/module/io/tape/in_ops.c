02c6cdf6b7bdb62ee191427688a8a27c53614de0
Clercin Guillaume <clercin.guillaume@gmail.com>
2012-05-09 19:17:36 +0200

    Rewrite io tape read

--- src/module/io/tape/in_ops.c
+++ src/module/io/tape/in_ops.c
@@ -27,7 +27,7 @@
 *                                                                           *
 *  -----------------------------------------------------------------------  *
 *  Copyright (C) 2012, Clercin guillaume <clercin.guillaume@gmail.com>      *
-*  Last modified: Thu, 22 Sep 2011 10:21:37 +0200                           *
+*  Last modified: Wed, 09 May 2012 19:15:43 +0200                           *
 \***************************************************************************/
 
 // errno
@@ -50,7 +50,7 @@ struct mtar_io_tape_in {
 
 	char * buffer;
 	unsigned int buffer_size;
-	unsigned int buffer_used;
+	unsigned int block_size;
 	char * buffer_pos;
 };
 
@@ -139,91 +139,47 @@ off_t mtar_io_tape_in_pos(struct mtar_io_in * io) {
 ssize_t mtar_io_tape_in_read(struct mtar_io_in * io, void * data, ssize_t length) {
 	struct mtar_io_tape_in * self = io->data;
 
-	ssize_t available = self->buffer_used - (self->buffer_pos - self->buffer);
-	if (available > 0) {
-		if (available == length) {
-			memcpy(data, self->buffer_pos, length);
+	ssize_t nb_total_read = self->block_size - (self->buffer_pos - self->buffer);
+	if (nb_total_read > 0) {
+		ssize_t will_copy = nb_total_read > length ? length : nb_total_read;
+		memcpy(data, self->buffer_pos, will_copy);
 
-			self->pos += length;
-			self->buffer_used = 0;
-			self->buffer_pos = self->buffer;
+		self->buffer_pos += will_copy;
+		self->pos += will_copy;
+
+		if (will_copy == length)
 			return length;
-		} else if (available > length) {
-			memcpy(data, self->buffer_pos, length);
+	}
 
-			self->pos += length;
-			self->buffer_pos += length;
-			return length;
-		} else {
-			ssize_t need_read = length - available;
-			if (need_read % self->buffer_size)
-				need_read += (self->buffer_size - available) % self->buffer_size;
-
-			char * buffer = malloc(need_read);
-			ssize_t nbRead = read(self->fd, buffer, need_read);
-
-			if (nbRead >= 0) {
-				memcpy(data, self->buffer_pos, available);
-				self->pos += available;
-				self->buffer_used = 0;
-				self->buffer_pos = self->buffer;
-
-				if (nbRead > 0) {
-					ssize_t copy_size = length - available;
-					char * pdata = data;
-					if (need_read > copy_size) {
-						memcpy(pdata + available, buffer, copy_size);
-
-						memcpy(self->buffer, buffer + copy_size, nbRead - copy_size);
-						self->pos += copy_size;
-						self->buffer_used = nbRead - copy_size;
-					} else
-						memcpy(pdata + available, buffer, copy_size);
-
-					free(buffer);
-
-					return length;
-				} else {
-					free(buffer);
-					return available;
-				}
-			} else {
-				free(buffer);
-				self->last_errno = errno;
-				return nbRead;
-			}
-		}
-	} else if (length % self->buffer_size == 0) {
-		ssize_t nbRead = read(self->fd, data, length);
-
-		if (nbRead > 0)
-			self->pos += length;
-		else if (nbRead < 0)
+	char * c_data = data;
+	while (length - nb_total_read >= self->block_size) {
+		ssize_t nb_read = read(self->fd, c_data + nb_total_read, self->block_size);
+		if (nb_read < 0) {
 			self->last_errno = errno;
+			return nb_read;
+		} else if (nb_read > 0) {
+			nb_total_read += nb_read;
+			self->pos += nb_read;
+		} else {
+			return nb_total_read;
+		}
+	}
 
-		return nbRead;
+	if (nb_total_read == length)
+		return length;
+
+	ssize_t nb_read = read(self->fd, self->buffer, self->block_size);
+	if (nb_read < 0) {
+		self->last_errno = errno;
+		return nb_read;
+	} else if (nb_read > 0) {
+		ssize_t will_copy = length - nb_total_read;
+		memcpy(data, self->buffer, will_copy);
+		self->buffer_pos = self->buffer + will_copy;
+		self->pos += will_copy;
+		return length;
 	} else {
-		ssize_t need_read = length - available + (self->buffer_size - available) % self->buffer_size;
-		char * buffer = malloc(need_read);
-		ssize_t nbRead = read(self->fd, buffer, need_read);
-
-		if (nbRead > 0) {
-			memcpy(data, buffer, length);
-			self->pos += length;
-
-			memcpy(self->buffer, buffer + length, need_read - length);
-			self->buffer_used = need_read - length;
-
-			free(buffer);
-			return nbRead;
-		} else if (nbRead == 0) {
-			free(buffer);
-			return 0;
-		} else {
-			free(buffer);
-			self->last_errno = errno;
-			return nbRead;
-		}
+		return nb_total_read;
 	}
 }
 
@@ -235,7 +191,7 @@ struct mtar_io_in * mtar_io_tape_new_in(int fd, int flags __attribute__((unused)
 
 	data->buffer = malloc(option->block_factor << 9);
 	data->buffer_size = option->block_factor << 9;
-	data->buffer_used = 0;
+	data->block_size = 0;
 	data->buffer_pos = data->buffer;
 
 	struct mtar_io_in * io = malloc(sizeof(struct mtar_io_in));
